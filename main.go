91c667370fe92dab53a2a6b85e3a2bb0aeee3420
乾 陈 <1769234376@qq.com>
2019-05-26 20:39:04 +0800

    push文件search.go

--- main.go
+++ main.go
@@ -9,13 +9,14 @@ import (
 
 func main() {
 	if err := dal.InitDB("admin:password@tcp(119.29.111.64)/testdb"); err != nil {
-		fmt.Println("Db error:%v",err)
+		fmt.Println("Db error:%v", err)
 		return
 	}
 
 	r := gin.Default()
 
-	r.GET("/like_num",HandleGetLikeNum)
+	r.GET("/like_num", HandleGetLikeNum)
+	r.GET("/search:Mname_Or_Singer", HandleSearch)
 	fmt.Printf("Ready!")
 	r.Run()
 }
---
+++ search.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	//"time"
+	"database/sql"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"strconv"
+	_ "strconv"
+	"time"
+)
+
+var index string
+
+type result struct {
+	id       int
+	mid      int
+	mname    string
+	singer   string
+	lrc      string
+	url      string
+	createAt time.Time
+	updateAt time.Time
+}
+
+var songResult = result{}
+
+func HandleSearch(c *gin.Context) {
+
+	db, err := sql.Open("mysql", "admin:testdb123456@tcp(119.29.111.64:3306)/testdb?parseTime=true")
+	checkErr(err)
+	index = c.Param("Mname_Or_Singer")
+	//查询数据
+	rows, err := db.Query("SELECT * FROM songs WHERE mname = '" + index + "' OR singer = '" + index + "' ")
+	checkErr(err)
+	printSearch(rows, c)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func printSearch(rows *sql.Rows, c *gin.Context) {
+
+	for rows.Next() {
+		var (
+			id       int
+			mid      int
+			mname    string
+			singer   string
+			lrc      string
+			url      string
+			createAt time.Time
+			updateAt time.Time
+		)
+		err := rows.Scan(&id, &mid, &mname, &singer, &lrc, &url, &createAt, &updateAt)
+		checkErr(err)
+		fmt.Println(id)
+		songResult.id = id
+		fmt.Println(mid)
+		songResult.mid = mid
+		fmt.Println(mname)
+		songResult.mname = mname
+		fmt.Println(singer)
+		songResult.singer = singer
+		fmt.Println(lrc)
+		songResult.lrc = lrc
+		fmt.Println(url)
+		songResult.url = url
+		fmt.Println(createAt)
+		songResult.createAt = createAt
+		fmt.Println(updateAt)
+		songResult.updateAt = updateAt
+		c.String(200, strconv.Itoa(songResult.id)+"%"+strconv.Itoa(songResult.mid)+"%"+songResult.mname+"%"+songResult.singer+"%"+songResult.url+"%"+songResult.lrc+"\n")
+	}
+}
