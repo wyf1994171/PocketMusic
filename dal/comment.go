247c79d0ed8f7d3f47ce3204727010cc580aa43a
richardevan <38094387+richardevan@users.noreply.github.com>
2019-05-29 21:39:07 +0800

    Update comment.go

--- dal/comment.go
+++ dal/comment.go
@@ -2,30 +2,29 @@ package dal
 
 import (
 	"PocketMusic/dal/model"
-	"fmt"
+	"database/sql"
 	_ "github.com/Go-SQL-Driver/mysql"
 	"time"
 )
 
-func main(){
-	if err := InitDB("admin:testdb123456@tcp(119.29.111.64)/testdb"); err != nil {
-		fmt.Println("Db error:%v",err)
-		return
+func checkErr(err error){
+	if err != nil {
+		panic(err)
 	}
 }
 
 func GetAllComment(Mid uint) ([]map[string]interface{}, error){
-
+	db, err := sql.Open("mysql", "admin:testdb123456@tcp(119.29.111.64:3306)/testdb?parseTime=true")
+	checkErr(err)
 	whereParams := make(map[string]interface{})
 	whereParams["mid"] = Mid
 	whereParams["status"] = 0
-	condition := CombineCondition(whereParams)
 	var comments []*model.Comment
-	err := db.Table("comments").Where(condition).Order("updated_at desc").Error
+	err, _ = db.Query("select uid, content from comments where mid = Mid and status = 0")
+	checkErr(err)
 	result := make([]map[string]interface{}, 0)
 	for key := range comments{
 		newComment := make(map[string]interface{})
-		newComment["mid"] = comments[key].MID
 		newComment["uid"] = comments[key].UID
 		newComment["content"] = comments[key].Content
 		result = append(result,newComment)
@@ -39,9 +38,9 @@ func CreateComment(Uid,Mid uint, content string) (uint, error)  {
 			CreatedAt: time.Now().Local(),
 			UpdatedAt: time.Now().Local(),
 		},
-		Content:  content,
 		UID:   Uid,
 		MID:   Mid,
+		Content:  content,
 		Status: 0,
 	}
 	err := db.Save(comment).Error
