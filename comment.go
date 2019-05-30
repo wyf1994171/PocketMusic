6b5a7fb7744e5b98698fb29135ce64933b2ce7ae
qianlAIyf <37335349+qianlAIyf@users.noreply.github.com>
2019-05-30 21:20:59 +0800

    Update comment.go

--- comment.go
+++ comment.go
@@ -2,26 +2,30 @@ package main
 
 import (
 	"PocketMusic/dal"
-	"PocketMusic/dal/model"
-	_ "github.com/Go-SQL-Driver/mysql"
 	"github.com/gin-gonic/gin"
 )
 
+type CommentForm struct {
+	Uid uint `json:"uid" form:"uid"`
+	Mid	uint `json:"mid" form:"mid"`
+	Content string `json:"content" form:"content"`
+}
+
 func HandleGetAllComment(c *gin.Context) {
-	var req model.Comment
+	var req CommentForm
 	if err := c.Bind(&req); err != nil {
 		writeResponse(c,-1,err.Error(),nil)
 		return
 	}
-	res, err := dal.GetAllComment(req.MID)
-	for key := range res {
-		name, err := dal.GetUserInfoByUid(res[key]["uid"].(string))
-		if err != nil {
-			writeResponse(c,-1,err.Error(),nil)
-			return
-		}
-		res[key]["name"] = name
-	}
+	res, err := dal.GetAllComment(req.Mid)
+	//for key := range res {
+	//	name, err := dal.GetUserInfoByUid(res[key]["uid"].(string))
+	//	if err != nil {
+	//		writeResponse(c,-1,err.Error(),nil)
+	//		return
+	//	}
+	//	res[key]["name"] = name
+	//}
 	if err != nil {
 		writeResponse(c,-1,err.Error(),nil)
 		return
@@ -30,12 +34,12 @@ func HandleGetAllComment(c *gin.Context) {
 }
 
 func HandleCreateComment(c *gin.Context){
-	var req model.Comment
+	var req CommentForm
 	if err := c.Bind(&req); err != nil {
 		writeResponse(c,-1,err.Error(),nil)
 		return
 	}
-	err := dal.CreateComment(req.UID,req.MID,req.Content)
+	err := dal.CreateComment(req.Uid,req.Mid,req.Content)
 	if err != nil {
 		writeResponse(c,-1,err.Error(),nil)
 		return
